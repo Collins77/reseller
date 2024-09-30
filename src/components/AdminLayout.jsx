8cbdf5b19d54b2f869d87fbbab9332c6e5830815
Collins77 <collinsmuemah@gmail.com>
2024-09-30 07:32:25 +0300

    admin-start

---
+++ src/components/AdminLayout.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import Sidebar from './SupplierSidebar';
+import Navbar from './SupplierNavbar';
+
+const AdminLayout = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <Sidebar isOpen={isSidebarOpen} />
+
+      {/* Main Content */}
+      <div className={`flex flex-col flex-grow transition-all duration-300 ${isSidebarOpen ? 'ml-64' : 'ml-16'}`}>
+        <Navbar toggleSidebar={toggleSidebar} isOpen={isSidebarOpen} />
+        
+        <main className="flex-grow p-4 bg-gray-100">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default AdminLayout;
